494642bf445182fedeb1e8d1fff57f5f0ab98d55
laurent <laurent.copey@gmail.com>
2024-04-07 18:37:17 +0200

    working physics ?

--- label_position/annotate.py
+++ label_position/annotate.py
@@ -1,185 +1,66 @@
 import numpy as np
-from plotly.colors import DEFAULT_PLOTLY_COLORS
 
-import math
 from dataclasses import dataclass, field
-from enum import Enum, auto
-
-__all__ = ['Point', 'DeterminantResult', 'Triangle', 'Annotation']
-
-
-@dataclass
-class Point:
-    x: float
-    y: float
-
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
-
-    def __sub__(self, other):
-        return Point(self.x - other.x, self.y - other.y)
-
-    def __neg__(self):
-        return Point(-self.x, -self.y)
-
-    def __mul__(self, other):
-        return Point(self.x * other, self.y * other)
-
-    def __truediv__(self, other: float):
-        return Point(self.x / other, self.y / other)
-
-    def dot(self, other):
-        return self.x * other.x + self.y * other.y
-
-    def distance(self):
-        return math.sqrt(self.l2())
-
-    def l2(self):
-        return self.x ** 2 + self.y ** 2
-
-
-class DeterminantResult(Enum):
-    left = auto()
-    right = auto()
-    colinear = auto()
-
-
-def determinant(left: Point, right: Point):
-    return left.x * right.y - left.y * right.x
-
-
-def _relative_position(vector: tuple[Point, Point], other: Point):
-    a, b = vector
-    d = b - a
-    t = other - a
-    result = determinant(d, t)
-    if result > 0:
-        return DeterminantResult.left
-    elif result < 0:
-        return DeterminantResult.right
-    else:
-        return DeterminantResult.colinear
-
-
-def line_intersect(edge: tuple[Point, Point], other: tuple[Point, Point]):
-    """
-    judge if line (v1,v2) intersects with line(v3,v4)
-    """
-    v1, v2 = edge
-    v3, v4 = other
-    d = (v4.y - v3.y) * (v2.x - v1.x) - (v4.x - v3.x) * (v2.y - v1.y)
-    u = (v4.x - v3.x) * (v1.y - v3.y) - (v4.y - v3.y) * (v1.x - v3.x)
-    v = (v2.x - v1.x) * (v1.y - v3.y) - (v2.y - v1.y) * (v1.x - v3.x)
-    if d < 0:
-        u, v, d = -u, -v, -d
-    return (0 <= u <= d) and (0 <= v <= d)
-
-
-@dataclass
-class Triangle:
-    """Les points doivent être dans l'ordre
-    de manière à ce que la gauche représente l'intérieur du triangle"""
-
-    a: Point
-    b: Point
-    c: Point
-
-    @property
-    def vertices(self):
-        return (self.a, self.b, self.c)
-
-    @property
-    def edges(self):
-        return ((self.a, self.b), (self.b, self.c), (self.c, self.a))
-
-    def point_in_triangle(self, other: "Triangle"):
-        # vérifie si l'un des points de other se trouve
-        # tous à gauche de toutes les edges de self
-        for vertex in other.vertices:
-            if all(
-                    [
-                        _relative_position(edge, vertex) == DeterminantResult.left
-                        for edge in self.edges
-                    ]
-            ):
-                return True
-        return False
-
-    def line_intersect(self, other: "Triangle"):
-        for other_edge in other.edges:
-            for self_edge in self.edges:
-                if line_intersect(other_edge, self_edge):
-                    return True
-        else:
-            return False
-
-    def plot(self, fig, line_color=None, **kwargs):
-        x = [vertex.x for vertex in [*self.vertices, self.vertices[0]]]
-        y = [vertex.y for vertex in [*self.vertices, self.vertices[0]]]
-        text = ["a", "b", "c", ""]
-        fig.add_scatter(
-            x=x,
-            y=y,
-            text=text,
-            line_color=line_color
-            if line_color is not None
-            else DEFAULT_PLOTLY_COLORS[0],
-            mode="lines+text",
-            **kwargs
-        )
-        return self
 
 
 @dataclass
 class Annotation:
-    point: Point
-    theta: float
-    l: float
+    x: float
+    y: float
+    margin: float
+    font_size: float
     text: str
-    font_size: int
-    in_collision: bool = False
-    bbox: Triangle = None
-    text_position: Point = None
-    force: Point = field(default_factory=lambda: Point(0, 0))
 
-    def __post_init__(self):
-        self.text = str(self.text)
-        self.set_text_position()
-
-    def set_text_position(self):
-        ax = np.cos(self.theta) * self.l
-        ay = np.sin(self.theta) * self.l
-        text_position = Point(self.point.x + ax, self.point.y + ay)
-        self.text_position = text_position
-
-        right = text_position + Point(self.font_size * (len(self.text) // 2), 0)
-        left = text_position - Point(self.font_size * (len(self.text) // 2), 0)
-        if 0 < self.theta % (2 * np.pi) < np.pi:
-            self.bbox = Triangle(self.point, right, left)
-        else:
-            self.bbox = Triangle(self.point, left, right)
-
-    def set_angle(self, theta):
-        self.theta = theta
-        self.set_text_position()
-
-    def plot_bbox(self, fig, **kwargs):
-        self.bbox.plot(fig, **kwargs, showlegend=False)
-        return self
-
-    def annotate(self, fig):
-        ax = self.point.x + np.cos(self.theta) * self.l
-        ay = self.point.y + np.sin(self.theta) * self.l
-        fig.add_annotation(
-            x=self.point.x,
-            y=self.point.y,
-            text=self.text,
-            ax=ax,
-            ay=ay,
-            axref="x",
-            ayref="y",
-        )
-        return self
-
-    def collide(self, other: "Annotation"):
-        return self.bbox.line_intersect(other.bbox)
+# @dataclass
+# class Annotation:
+#     point: Point
+#     theta: float
+#     l: float
+#     text: str
+#     font_size: int
+#     in_collision: bool = False
+#     bbox: Triangle = None
+#     text_position: Point = None
+#     force: Point = field(default_factory=lambda: Point(0, 0))
+#
+#     def __post_init__(self):
+#         self.text = str(self.text)
+#         self.set_text_position()
+#
+#     def set_text_position(self):
+#         ax = np.cos(self.theta) * self.l
+#         ay = np.sin(self.theta) * self.l
+#         text_position = Point(self.point.x + ax, self.point.y + ay)
+#         self.text_position = text_position
+#
+#         right = text_position + Point(self.font_size * (len(self.text) // 2), 0)
+#         left = text_position - Point(self.font_size * (len(self.text) // 2), 0)
+#         if 0 < self.theta % (2 * np.pi) < np.pi:
+#             self.bbox = Triangle(self.point, right, left)
+#         else:
+#             self.bbox = Triangle(self.point, left, right)
+#
+#     def set_angle(self, theta):
+#         self.theta = theta
+#         self.set_text_position()
+#
+#     def plot_bbox(self, fig, **kwargs):
+#         self.bbox.plot(fig, **kwargs, showlegend=False)
+#         return self
+#
+#     def annotate(self, fig):
+#         ax = self.point.x + np.cos(self.theta) * self.l
+#         ay = self.point.y + np.sin(self.theta) * self.l
+#         fig.add_annotation(
+#             x=self.point.x,
+#             y=self.point.y,
+#             text=self.text,
+#             ax=ax,
+#             ay=ay,
+#             axref="x",
+#             ayref="y",
+#         )
+#         return self
+#
+#     def collide(self, other: "Annotation"):
+#         return self.bbox.line_intersect(other.bbox)
--- label_position/physics.py
+++ label_position/physics.py
@@ -1,55 +1,307 @@
-from .annotate import Point, Annotation, Triangle
-import numpy as np
+from .annotate import Annotation
 import math
+from dataclasses import dataclass, field
+import plotly.graph_objs as go
+from plotly.colors import DEFAULT_PLOTLY_COLORS
 
 
-def radial_force(a, b, k):
-    vector = b - a
-    distance = vector.distance()
-    unit_vector = vector / distance
-    return unit_vector * k / distance
+def get_arrow(at: 'Point', vector: 'Point'):
+    tip = at + vector
+    x = at.x, tip.x
+    y = at.y, tip.y
+    yield x, y
+    d_theta = math.pi * 10 / 180
+    l = 0.1
+    base = (vector * l).rotate(d_theta)
+    x = tip.x, tip.x - base.x
+    y = tip.y, tip.y - base.y
+    yield x, y
+    base = (vector * l).rotate(-d_theta)
+    x = tip.x, tip.x - base.x
+    y = tip.y, tip.y - base.y
+    yield x, y
 
 
-def force_between_triangle(left: Triangle, right: Triangle):
-    # cherche la plus petite distance
-    # projection des vertices de left sur les edges de right
-    for vertex in left.vertices:
-        for edge in right.edges:
-            projection = vertex.dot(edge[1] - edge[0])
-            # distance à la droite
-            distance = math.sqrt((edge[0] - vertex).l2() - projection ** 2)
+@dataclass
+class Point:
+    x: float
+    y: float
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __neg__(self):
+        return Point(-self.x, -self.y)
+
+    def __mul__(self, other):
+        return Point(self.x * other, self.y * other)
+
+    def __truediv__(self, other: float):
+        return Point(self.x / other, self.y / other)
+
+    def __eq__(self, other: 'Point') -> bool:
+        return self.x == other.x and self.y == other.y
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y
+
+    def norm(self):
+        return math.sqrt(self.l2())
+
+    def l2(self):
+        return self.x ** 2 + self.y ** 2
+
+    def rotate(self, theta):
+        return Point(self.x * math.cos(theta) - self.y * math.sin(theta),
+                     self.x * math.sin(theta) + self.y * math.cos(theta))
+
+    def plot_at(self, fig: go.Figure, at: 'Point', **kwargs):
+        line_color = kwargs.pop('line_color', DEFAULT_PLOTLY_COLORS[0])
+        for x, y in get_arrow(at, self):
+            fig.add_scatter(x=x, y=y, showlegend=False, mode='lines', line_color=line_color, **kwargs)
+        return self
 
 
-def compute_angle_with_radial_contraint(
-        position: Point, force: Point, center: Point, radius: float
-):
-    new_position = position + force
-    return math.atan2(new_position.y, new_position.x)
+@dataclass
+class AABB:
+    position: Point
+    width: float
+    height: float
+
+    @property
+    def vertices(self):
+        return (
+            Point(self.position.x + self.width / 2, self.position.y - self.height / 2),
+            Point(self.position.x + self.width / 2, self.position.y + self.height / 2),
+            Point(self.position.x - self.width / 2, self.position.y + self.height / 2),
+            Point(self.position.x - self.width / 2, self.position.y - self.height / 2)
+        )
+
+    @property
+    def y_max(self):
+        return self.position.y + self.height / 2
+
+    @property
+    def y_min(self):
+        return self.position.y - self.height / 2
+
+    @property
+    def x_max(self):
+        return self.position.x + self.width / 2
+
+    @property
+    def x_min(self):
+        return self.position.x - self.width / 2
+
+    def collide(self, other: 'AABB') -> bool:
+        y = (self.y_min < other.y_max < self.y_max) or (self.y_min < other.y_min < self.y_max)
+        x = (self.x_min < other.x_max < self.x_max) or (self.x_min < other.x_min < self.x_max)
+        return x and y
+
+    def collision_force(self, other: 'AABB') -> Point:
+        if self.collide(other):
+            direction = self.position - other.position
+            return direction / direction.norm()
+
+        return Point(0, 0)
+
+    def plot(self, fig: go.Figure, **kwargs):
+        points = (*self.vertices, self.vertices[0])
+        line_color = kwargs.pop('line_color', DEFAULT_PLOTLY_COLORS[0])
+        for n, point in enumerate(points[:-1]):
+            next_point = points[n + 1]
+            x = point.x, next_point.x
+            y = point.y, next_point.y
+
+            fig.add_scatter(x=x, y=y, mode='lines', line_color=line_color, showlegend=False, **kwargs)
+
+        return self
 
 
-def optimize_annotation_position(points: list[Annotation], k=1, max_iter: int = 100):
-    previous_norm = 1
-    norm_variation = 1
+@dataclass
+class AnnotationWithPhysics(Annotation):
+    bbox: AABB = None
+    force: Point = field(default_factory=lambda: Point(0, 0))
+    velocity: Point = field(default_factory=lambda: Point(0, 0))
+    damping: float = 0.9
+    spring: float = 0.1
+    offset: float = 1
 
-    iteration = 0
-    while norm_variation > 1e-2 and iteration < max_iter:
-        sum_forces = Point(0, 0)
-        for n, point1 in enumerate(points):
-            for point2 in points[n + 1:]:
-                force = radial_force(point1.text_position, point2.text_position, k)
-                point1.force -= force
-                point2.force += force
-                sum_forces += force
+    def __post_init__(self):
+        self.bbox = AABB(
+            position=Point(self.x, self.y),
+            width=(self.font_size + 2 * self.margin) * len(self.text),
+            height=self.font_size + 2 * self.margin
+        )
 
-        current_norm = sum_forces.distance()
-        norm_variation = abs(current_norm - previous_norm)
-        print(norm_variation)
-        for point in points:
-            next_angle = compute_angle_with_radial_contraint(
-                point.text_position, point.force, point.point, point.l
-            )
-            point.force = Point(0, 0)
-            print(f'angle {(next_angle - point.theta) * 360 / (2 * np.pi)}', )
-            point.set_angle(next_angle)
-        previous_norm = current_norm
-        iteration += 1
+    def plot_bbox(self, fig, **kwargs):
+        return self.bbox.plot(fig, **kwargs)
+
+    def collide(self, other: 'AnnotationWithPhysics'):
+        force = self.bbox.collision_force(other.bbox)
+        self.force += force
+        other.force -= force
+
+    def spring_force(self):
+        direction = self.bbox.position - Point(self.x, self.y)
+        norm = direction.norm() + 1e-6
+        return (direction / norm) * -self.spring * (norm - self.offset)
+
+    def apply_forces(self, dt: float):
+        self.force += self.spring_force()
+        self.velocity += self.force * dt
+        self.bbox.position += self.velocity * dt
+        self.velocity *= self.damping
+
+    def reset_force(self):
+        self.force = Point(0, 0)
+
+    def step(self, dt: float = 0.1):
+        self.apply_forces(dt)
+        self.reset_force()
+
+# class DeterminantResult(Enum):
+#     left = auto()
+#     right = auto()
+#     colinear = auto()
+#
+#
+# def determinant(left: Point, right: Point):
+#     return left.x * right.y - left.y * right.x
+#
+#
+# def _relative_position(vector: tuple[Point, Point], other: Point):
+#     a, b = vector
+#     d = b - a
+#     t = other - a
+#     result = determinant(d, t)
+#     if result > 0:
+#         return DeterminantResult.left
+#     elif result < 0:
+#         return DeterminantResult.right
+#     else:
+#         return DeterminantResult.colinear
+#
+#
+# def line_intersect(edge: tuple[Point, Point], other: tuple[Point, Point]):
+#     """
+#     judge if line (v1,v2) intersects with line(v3,v4)
+#     """
+#     v1, v2 = edge
+#     v3, v4 = other
+#     d = (v4.y - v3.y) * (v2.x - v1.x) - (v4.x - v3.x) * (v2.y - v1.y)
+#     u = (v4.x - v3.x) * (v1.y - v3.y) - (v4.y - v3.y) * (v1.x - v3.x)
+#     v = (v2.x - v1.x) * (v1.y - v3.y) - (v2.y - v1.y) * (v1.x - v3.x)
+#     if d < 0:
+#         u, v, d = -u, -v, -d
+#     return (0 <= u <= d) and (0 <= v <= d)
+#
+#
+# @dataclass
+# class Triangle:
+#     """Les points doivent être dans l'ordre
+#     de manière à ce que la gauche représente l'intérieur du triangle"""
+#
+#     a: Point
+#     b: Point
+#     c: Point
+#
+#     @property
+#     def vertices(self):
+#         return (self.a, self.b, self.c)
+#
+#     @property
+#     def edges(self):
+#         return ((self.a, self.b), (self.b, self.c), (self.c, self.a))
+#
+#     def point_in_triangle(self, other: "Triangle"):
+#         # vérifie si l'un des points de other se trouve
+#         # tous à gauche de toutes les edges de self
+#         for vertex in other.vertices:
+#             if all(
+#                     [
+#                         _relative_position(edge, vertex) == DeterminantResult.left
+#                         for edge in self.edges
+#                     ]
+#             ):
+#                 return True
+#         return False
+#
+#     def line_intersect(self, other: "Triangle"):
+#         for other_edge in other.edges:
+#             for self_edge in self.edges:
+#                 if line_intersect(other_edge, self_edge):
+#                     return True
+#         else:
+#             return False
+#
+#     def plot(self, fig, line_color=None, **kwargs):
+#         x = [vertex.x for vertex in [*self.vertices, self.vertices[0]]]
+#         y = [vertex.y for vertex in [*self.vertices, self.vertices[0]]]
+#         text = ["a", "b", "c", ""]
+#         fig.add_scatter(
+#             x=x,
+#             y=y,
+#             text=text,
+#             line_color=line_color
+#             if line_color is not None
+#             else DEFAULT_PLOTLY_COLORS[0],
+#             mode="lines+text",
+#             **kwargs
+#         )
+#         return self
+#
+#
+# def radial_force(a, b, k):
+#     vector = b - a
+#     distance = vector.distance()
+#     unit_vector = vector / distance
+#     return unit_vector * k / distance
+#
+#
+# def force_between_triangle(left: Triangle, right: Triangle):
+#     # cherche la plus petite distance
+#     # projection des vertices de left sur les edges de right
+#     for vertex in left.vertices:
+#         for edge in right.edges:
+#             projection = vertex.dot(edge[1] - edge[0])
+#             # distance à la droite
+#             distance = math.sqrt((edge[0] - vertex).l2() - projection ** 2)
+#
+#
+# def compute_angle_with_radial_constraint(
+#         position: Point, force: Point, center: Point, radius: float
+# ):
+#     new_position = position + force
+#     return math.atan2(new_position.y, new_position.x)
+#
+#
+# def optimize_annotation_position(points: list[Annotation], k=1, max_iter: int = 100):
+#     previous_norm = 1
+#     norm_variation = 1
+#
+#     iteration = 0
+#     while norm_variation > 1e-2 and iteration < max_iter:
+#         sum_forces = Point(0, 0)
+#         for n, point1 in enumerate(points):
+#             for point2 in points[n + 1:]:
+#                 force = radial_force(point1.text_position, point2.text_position, k)
+#                 point1.force -= force
+#                 point2.force += force
+#                 sum_forces += force
+#
+#         current_norm = sum_forces.distance()
+#         norm_variation = abs(current_norm - previous_norm)
+#         print(norm_variation)
+#         for point in points:
+#             next_angle = compute_angle_with_radial_constraint(
+#                 point.text_position, point.force, point.point, point.l
+#             )
+#             point.force = Point(0, 0)
+#             print(f'angle {(next_angle - point.theta) * 360 / (2 * np.pi)}', )
+#             point.set_angle(next_angle)
+#         previous_norm = current_norm
+#         iteration += 1
